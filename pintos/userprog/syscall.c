b8b67d64f50d06ddb522ddb4aa62c0fd3ce923de
kda <1325680q@gmail.com>
2026-05-09 21:56:52 +0900

    Revert "fix: exec 실패 시 -1 반환으로 제어 흐름 복구"
    This reverts commit e6033e2b761d352d16997f4aac0d967ab024ece9.

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -474,8 +474,11 @@ sys_filesize (const int fd) {
 
 int
 sys_exec (char *file) {
-	if(!check_file_name(file) || process_exec (file) < 0) {
+	if(!check_file_name(file)) {
 		return -1;
+	}
+	if (process_exec (file) < 0) {
+		syscall_exit(-1);
 	}
 	NOT_REACHED();
 }
